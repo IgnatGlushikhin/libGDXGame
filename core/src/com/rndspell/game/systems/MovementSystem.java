e0d6570d1c2173262bcf8c7b63dd48abba86fc0d
Сергей Бычков <bychkov.sergei.vladimirovich@gmail.com>
2014-07-23 14:18:29 +0400

    add random move

--- core/src/com/rndspell/game/systems/MovementSystem.java
+++ core/src/com/rndspell/game/systems/MovementSystem.java
@@ -3,11 +3,14 @@ package com.rndspell.game.systems;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
-import com.badlogic.gdx.math.Vector2;
 import com.rndspell.game.components.MovementComponent;
 import com.rndspell.game.components.PositionComponent;
 
+import java.util.Random;
+
 public class MovementSystem extends IteratingSystem {
+
+    private Random random = new Random();
 
     public MovementSystem() {
         super(Family.getFamilyFor(PositionComponent.class, MovementComponent.class));
@@ -17,6 +20,11 @@ public class MovementSystem extends IteratingSystem {
     public void processEntity(Entity entity, float deltaTime) {
         PositionComponent position = entity.getComponent(PositionComponent.class);
         MovementComponent movement = entity.getComponent(MovementComponent.class);
+
+        // для теста зададим движение в случайном направлении
+        if(random.nextInt(10) < 1){
+            movement.getAcceleration().set(movement.getAccelerationValue(), 0).rotate(random.nextInt(360));
+        }
 
         movement.getVelocity().add(movement.getAcceleration().cpy().scl(deltaTime));
         if(movement.getVelocity().len() > movement.getMaxVelocity()){
